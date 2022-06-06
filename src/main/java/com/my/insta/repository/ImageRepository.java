a3c297d55baeb9abaa10c0ee6b7204b4b8bf5967
yevhentabachynskyi <tayeiv92@gmail.com>
2022-06-06 15:06:17 +0300

    add ImageRepository

---
+++ src/main/java/com/my/insta/repository/ImageRepository.java
@@ -0,0 +1,12 @@
+package com.my.insta.repository;
+
+import com.my.insta.entity.ImageModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ImageRepository extends JpaRepository<ImageModel, Long> {
+    Optional<ImageModel> findByUserId(Long id);
+    Optional<ImageModel> findByPostId(Long id);
+
+}
