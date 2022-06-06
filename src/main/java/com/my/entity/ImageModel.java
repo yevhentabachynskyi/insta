3ee6621d917a604fc597af64d88b2e1323657dc6
yevhentabachynskyi <tayeiv92@gmail.com>
2022-06-06 12:33:55 +0300

    Image annotation

--- src/main/java/com/my/entity/ImageModel.java
+++ src/main/java/com/my/entity/ImageModel.java
@@ -1,10 +1,25 @@
 package com.my.entity;
 
+import lombok.Data;
+import net.minidev.json.annotate.JsonIgnore;
+
+import javax.persistence.*;
+
+@Data
+@Entity
 public class ImageModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Lob
+    @Column(columnDefinition = "LONGBLOB")
     private byte[] imageBytes;
+    @JsonIgnore
     private Long userId;
+    @JsonIgnore
     private Long postId;
 
 }
