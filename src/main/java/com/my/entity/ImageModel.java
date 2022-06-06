33968d2ae274f31c262c205da4aa94d6df684bb8
yevhentabachynskyi <tayeiv92@gmail.com>
2022-06-06 11:10:32 +0300

    entity ImageModel

--- src/main/java/com/my/entity/ImageModel.java
+++ src/main/java/com/my/entity/ImageModel.java
@@ -1,4 +1,10 @@
 package com.my.entity;
 
 public class ImageModel {
+    private Long id;
+    private String name;
+    private byte[] imageBytes;
+    private Long userId;
+    private Long postId;
+
 }
