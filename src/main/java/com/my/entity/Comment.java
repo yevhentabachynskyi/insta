20c36ae624edf3ae84d28ade85b2ff1557d2422e
yevhentabachynskyi <tayeiv92@gmail.com>
2022-06-06 12:29:59 +0300

    Comment annotation

--- src/main/java/com/my/entity/Comment.java
+++ src/main/java/com/my/entity/Comment.java
@@ -1,15 +1,26 @@
 package com.my.entity;
 
-import javax.persistence.PrePersist;
+import lombok.Data;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Data
+@Entity
 public class Comment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne(fetch = FetchType.EAGER)
     private Post post;
+    @Column(name = "username", nullable = false)
     private String userName;
+    @Column(nullable = false)
     private Long userId;
+    @Column(columnDefinition = "text", nullable = false)
     private String message;
+    @Column(updatable = false)
     private LocalDateTime createdDate;
 
     @PrePersist
