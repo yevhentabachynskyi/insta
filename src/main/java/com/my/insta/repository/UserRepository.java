38047d69a477ce8a76388ae17e049b573c8342cb
yevhentabachynskyi <tayeiv92@gmail.com>
2022-06-06 14:35:14 +0300

    add UserRepository

---
+++ src/main/java/com/my/insta/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.my.insta.repository;
+
+import com.my.insta.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByUserName(String userName);
+    Optional<User> findUserByEmail(String email);
+    Optional<User> findUserById(Long id);
+}
